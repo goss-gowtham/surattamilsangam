317afcd70c7148c58377afa9d53fe78e6e3cf927
Gowthamnarayanan S <gowtham@sumyag.com>
2020-12-20 22:43:30 +0530

    Publishing live with route fix on gh-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about/about.component';
 import { ContactComponent } from './about/contact/contact.component';
+import { EventsComponent } from './events/events/events.component';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [{
     path: 'home',
+    component: HomeComponent,
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
     data: { preload: true }
   }, {
     path: 'about',
+    component: AboutComponent,
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
   }, {
     path: 'events',
+    component: EventsComponent,
     loadChildren: () => import('./events/events.module').then(m => m.EventsModule),
   }, {
     path: 'contact',
--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsComponent } from './events/events.component';
 
-const routes: Routes = [{
-  path: '', component: EventsComponent
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
