00e54a3197748ca8e3d2db732c2aa7bf7934a983
Gowthamnarayanan S <gowtham@sumyag.com>
2020-12-20 22:13:43 +0530

    Router reload fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './about/contact/contact.component';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [{
     path: 'home',
@@ -17,8 +18,7 @@ const routes: Routes = [{
     component: ContactComponent
   }, {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
+    component: HomeComponent
   }];
 
 @NgModule({
