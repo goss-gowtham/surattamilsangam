0893a151664c94101c01743aa0930f72714da106
Gowthamnarayanan S <gowtham@sumyag.com>
2020-12-28 16:36:25 +0530

    Publishing Live V1.0

--- src/app/core/contact/contact.component.ts
+++ src/app/core/contact/contact.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  purpose = "";
+  validate: number;
+  random1 = Math.floor(Math.random() * 5) + 1;
+  random2 = Math.floor(Math.random() * 5) + 1;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/core/events/events.component.ts
+++ src/app/core/events/events.component.ts
@@ -24,6 +24,48 @@ export class EventsComponent implements OnInit {
     let data: any = await this.http.get(this.firebasePrefix + 'Data%2Fevents.json?alt=media&token=53dae2b8-3fd3-4762-844c-841fc998e50b').toPromise();
     this.events = data.events.sort((a, b) => b.year - a.year);
     this.events = data.events;
+    // Some random colors
+    const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+    const numBalls = 50;
+    const balls = [];
+
+    for (let i = 0; i < numBalls; i++) {
+      let ball = document.createElement("div");
+      ball.classList.add("ball");
+      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+      ball.style.transform = `scale(${Math.random()})`;
+      ball.style.width = `${Math.random()}em`;
+      ball.style.height = ball.style.width;
+
+      balls.push(ball);
+      document.getElementById('balls').append(ball);
+    }
+
+    // Keyframes
+    balls.forEach((el, i, ra) => {
+      let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
+      };
+
+      let anim = el.animate(
+        [
+          { transform: "translate(0, 0)" },
+          { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        ],
+        {
+          duration: (Math.random() + 1) * 5000, // random duration
+          direction: "alternate",
+          fill: "both",
+          iterations: Infinity,
+          easing: "ease-in-out"
+        }
+      );
+    });
+
   }
 
   openModal(event) {
